484bb155d2f144aa18f2f2747f6c8a42e76b6c51
Tim Weber <scy@scytale.name>
2011-10-12 12:22:43 +0200

    Add bottom links

--- app/locale/text/de.php
+++ app/locale/text/de.php
@@ -1,5 +1,12 @@
 <?php
 
 return array(
+	// Top
 	'subtitle' => 'Eine Ferienkolonie im grünen Herzen Frankreichs',
+	// Bottom
+	'bottom-house' => 'Das Haus',
+	'bottom-rencontres' => 'Der Verein',
+	'bottom-route-surroundings' => 'Anfahrt und Umgebung',
+	'bottom-booking' => 'Buchen',
+	'bottom-contact' => 'Kontakt',
 );
