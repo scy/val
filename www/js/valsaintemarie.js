6a4bd14689d4323750ceb05b99dacd2d71ef5852
Tim Weber <scy@scytale.name>
2011-11-26 10:13:51 +0100

    Remember scroll position on choose6 pages
    The position is remembered in a JavaScript-set cookie and restored on
    page load, if the cookie exists and the chooser div is positioned to the
    far left.
    
    There is one cookie per body ID.
    
    This feature is necessary at least for Chrome, since it doesn’t remember
    the horizontal scroll position on history movements. Firefox does it
    right.

--- www/js/valsaintemarie.js
+++ www/js/valsaintemarie.js
@@ -1,8 +1,23 @@
 (function ($) {
 	'use strict';
+	// Things that should run on page load.
+	var onload = [
+		// Remember scroll position on choose6 pages.
+		function ($) {
+			var name = 'scrollpos-' + $('body').attr('id');
+			var $div = $('body.choose6 #main');
+			$div.scroll(function (ev) {
+				$.cookie(name, $div.scrollLeft());
+			});
+			var value = $.cookie(name);
+			if (value !== null && $div.scrollLeft() == 0) {
+				$div.scrollLeft(value);
+			}
+		}
+	];
 	window.valsaintemarie = (function ($) {
 		return {
-			onload: []
+			onload: onload
 		}
 	})($);
 	$(function () {
